bc654e0893d87344156a1545cb1f446e443ebc96
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-14 22:17:21 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+import secrets
+
+class Config:
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = secrets.token_urlsafe(24)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://alejandro:seyerreyes@isladigital.xyz:3333/alejandrobd'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:contaseña@85.239.241.150:3306/alejandrobd'
+
+#Comandos para descargar en instalar todas las librerias offline
+#python ñ
+#pip install --no-index --find-links=librerias -r requirements.txt
