f8e225f22d091d4c53b1c6354c6cb26e2587fe34
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-16 21:15:55 -0500

    a

--- config.py
+++ config.py
@@ -5,10 +5,7 @@ load_dotenv()
 
 class Config:
     DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT", "3311")  # default MySQL port
-    DB_NAME = os.getenv("DB_NAME")
+ 
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
