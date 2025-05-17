b547105e1454af8e505baee6a4aeb03a0c62ecdb
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-16 21:50:38 -0500

    subir cambios

--- config.py
+++ config.py
@@ -5,13 +5,15 @@ load_dotenv()
 
 class Config:
     DB_USER = os.environ.get("DB_USER")
- 
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
-    print(DB_USER)
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:Seyerreyes27_@isladigital.xyz:3311/alejandrobd"
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:contaseña@85.239.241.150:3306/alejandrobd'
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 #Comandos para descargar en instalar todas las librerias offline
 #python ñ
