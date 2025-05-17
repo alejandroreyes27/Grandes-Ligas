dbf9ab60418e99f8f128b9d49f2952ebc37f986f
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-16 20:05:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,5 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{USUARIO}:{PASS}@isladigital.xyz:3311/alejandrobd'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://$usuario:$contaseña@85.239.241.150:3306/alejandrobd'
-
-#Comandos para descargar en instalar todas las librerias offline
-#python ñ
-#pip install --no-index --find-links=librerias -r requirements.txt
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{DB_USER}:{DB_PASS}@isladigital.xyz:3311/alejandrobd'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:contaseña@85.239.241.150:3306/alejandrobd'
