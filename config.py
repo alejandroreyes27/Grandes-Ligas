d44c3a4708f7ce151fd39d212a72b4049ad96500
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-16 21:52:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     DB_USER = os.environ.get("DB_USER")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_PASSWORD = os.environ.get("DB_PASS")
     DB_HOST = os.environ.get("DB_HOST")
     DB_PORT = os.environ.get("DB_PORT")
     DB_NAME = os.environ.get("DB_NAME")
