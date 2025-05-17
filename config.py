44649ab9f9d8de86fd69fc7da2145c283e68d888
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-16 22:29:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@isladigital.xyz:{DB_PORT}/{DB_NAME}"
 
 #Comandos para descargar en instalar todas las librerias offline
 #python ñ
